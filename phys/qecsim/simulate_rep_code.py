f5613310cf3cfd50b2232ad8fbb75a4554cfb96d
Lior Ella <lior@quantum-machines.co>
2022-01-03 17:07:57 +0200

    added active reset example

--- phys/qecsim/simulate_rep_code.py
+++ phys/qecsim/simulate_rep_code.py
@@ -1,18 +1,21 @@
+from collections import Iterable
+
 import quantumsim  # https://gitlab.com/quantumsim/quantumsim, branch: stable/v0.2
 import quantumsim.circuit as circuit
 import numpy as np
 import matplotlib.pyplot as plt
 
-t1 = 10e3
-t2 = 10e3
+t1 = 10e5
+t2 = 10e5
 single_qubit_gate_duration = 20
 two_qubit_gate_duration = 40
 meas_duration = 400
 reset_duration = 250
+reset_latency = 200
 
 circuits = {
     "no_err": circuit.Circuit(),
-    "data_bitflip_round": circuit.Circuit()
+    "data_bitflip": circuit.Circuit()
 }
 
 sampler = circuit.BiasedSampler(0, seed=42, alpha=1)
@@ -26,8 +29,15 @@ for c in circuits:
         circuits[c].add_qubit(qubits[q])
 
 # todo: make parametrized
-circuits["no_err"].add_qubit(circuit.ClassicalBit("ma0"))
+cbits = {
+    name: circuit.ClassicalBit(name) for name in {"ma0"}
+}
+for c in circuits:
+    for cb in cbits:
+        circuits[c].add_qubit(cbits[cb])
 
+# stabilizer round with no errors
+########
 t_start = 0
 t_moment = single_qubit_gate_duration
 circuits["no_err"].add_gate(circuit.RotateY("a0",
@@ -61,14 +71,78 @@ t_start += t_moment
 circuits["no_err"].add_waiting_gates(0, t_start)
 circuits["no_err"].order()
 
-fig, ax = circuits["no_err"].plot()
-fig.set_figwidth(15)
-plt.show()
+# fig, ax = circuits["no_err"].plot()
+# fig.set_figwidth(15)
+# plt.show()
 
+# inject data qubit error
+##################
+
+t_start = 0
+t_moment = single_qubit_gate_duration
+circuits["data_bitflip"].add_gate(circuit.RotateX("d0",
+                                                  t_start + t_moment / 2,
+                                                  np.pi))
+
+t_start += t_moment
+circuits["data_bitflip"].add_waiting_gates(0, t_start)
+circuits["data_bitflip"].order()
+
+
+# fig2, ax2 = circuits["data_bitflip"].plot()
+# fig2.set_figwidth(15)
+# plt.show()
+
+def active_reset_circ(qubits_to_reset: Iterable) -> circuit.Circuit:
+    arc = circuit.Circuit()
+    for q in qubits:
+        arc.add_qubit(qubits[q])
+    for cb in cbits:
+        arc.add_qubit(cbits[cb])
+
+    t_start = 0
+    t_moment = reset_latency
+
+    t_start += t_start
+    t_moment = single_qubit_gate_duration
+
+    for q in qubits_to_reset:
+        arc.add_gate(circuit.RotateX(q,
+                                     t_start + t_moment / 2,
+                                     np.pi))
+    t_start += t_moment
+    arc.add_waiting_gates(0, t_start)
+    arc.order()
+    # fig3, ax3 = arc.plot()
+    # fig.set_figwidth(10)
+    # plt.show()
+    return arc
+
+
+# run simulation
+########
 state = quantumsim.sparsedm.SparseDM(circuits["no_err"].get_qubit_names())
 results = []
 for i in range(3):
     circuits["no_err"].apply_to(state)
     results.append(state.classical["ma0"])
+    if state.classical["ma0"] == 1:
+        active_reset_circ(["a0"]).apply_to(state)
+
+circuits["data_bitflip"].apply_to(state)
+
+for i in range(6):
+    circuits["no_err"].apply_to(state)
+    results.append(state.classical["ma0"])
+    if state.classical["ma0"] == 1:
+        active_reset_circ(["a0"]).apply_to(state)
+
+circuits["data_bitflip"].apply_to(state)
+
+for i in range(6):
+    circuits["no_err"].apply_to(state)
+    results.append(state.classical["ma0"])
+    if state.classical["ma0"] == 1:
+        active_reset_circ(["a0"]).apply_to(state)
 
 print(results)
