465708f2855491923cedf05c88a468536efac36d
Lior Ella <lior@quantum-machines.co>
2022-01-03 16:33:15 +0200

    WIP on qecsim_lib (less important)

--- phys/qecsim/qecsim_lib.py
+++ phys/qecsim/qecsim_lib.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Callable
 
 import numpy as np
 from dataclasses import dataclass
@@ -22,6 +22,7 @@ class QECCirc:
         self.qreg = [0] * (2 * distance + 1)
         self.creg = [False] * creg_size
         self.error_probs = ErrorProbs() if error_probs is None else error_probs
+        self.circuit_ops = []
 
     def _cnot(self, qc, qt):
         cnot_table = {
@@ -68,32 +69,50 @@ class QECCirc:
         used_qubits = np.zeros(len(self.qreg))
         for op in ops_list:
             if op[0] == "CNOT":
-                self._cnot(op[1], op[2])
                 used_qubits[op[1]] += 1
                 used_qubits[op[2]] += 1
             if op[0] == "M":
-                self._measure(op[1], op[2])
                 used_qubits[op[1]] += 1
             if op[0] == "WAIT":
-                self._wait(op[1], op[2])
                 used_qubits[op[1]] += 1
-            if op[0] == "CL_NOT":
-                self._cl_not(op[1])
-            if op[0] == "CL_XOR":
-                self._cl_xor(op[1], op[2], op[3])
             if op[0] == "X":
-                self._x(op[1])
                 used_qubits[op[1]] += 1
         if not np.all(used_qubits == 1):
             raise AttributeError("every qubit must be used in every moment exactly once")
+        self.circuit_ops.append(ops_list)
+
+    def run(self, moment_callback: Optional[Callable] = None):
+        for ops_list in self.circuit_ops:
+            for op in ops_list:
+                if op[0] == "CNOT":
+                    self._cnot(op[1], op[2])
+                if op[0] == "M":
+                    self._measure(op[1], op[2])
+                if op[0] == "WAIT":
+                    self._wait(op[1], op[2])
+                if op[0] == "CL_NOT":
+                    self._cl_not(op[1])
+                if op[0] == "CL_XOR":
+                    self._cl_xor(op[1], op[2], op[3])
+                if op[0] == "X":
+                    self._x(op[1])
+            moment_callback(self)
 
 
 if __name__ == "__main__":
     d = 2
-    cq = QECCirc(d, 2*d+1)
-    cq.add_moment([("X", 0)] + [("WAIT", q, 20) for q in range(1, 2*d+1)])
+    cq = QECCirc(d, 2 * d + 1)
+    cq.add_moment([("X", 0)] + [("WAIT", q, 20) for q in range(1, 2 * d + 1)])
     cq.add_moment([("CNOT", 0, 1), ("CNOT", 2, 3), ("WAIT", 4, 30)])
     cq.add_moment([("CNOT", 4, 3), ("CNOT", 2, 1), ("WAIT", 0, 30)])
     cq.add_moment([("WAIT", 0, 400), ("WAIT", 2, 400), ("WAIT", 4, 400), ("M", 1, 1), ("M", 3, 3)])
+
+
+    def callback(self):
+        print("dq = ", self.qreg[::2], "\tda = ", self.qreg[1::2], "cr = ", self.creg)
+
+
+    cq.run(callback)
+
     print("qreg = ", cq.qreg)
     print("creg = ", cq.creg)
