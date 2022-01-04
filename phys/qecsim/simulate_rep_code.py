7449c4b90e3bb146bd4e2dbf4fc7afd0ca1dfa4e
Lior Ella <lior@quantum-machines.co>
2022-01-04 17:29:32 +0200

    example that includes error recovery

--- phys/qecsim/simulate_rep_code.py
+++ phys/qecsim/simulate_rep_code.py
@@ -5,6 +5,9 @@ import quantumsim  # https://gitlab.com/quantumsim/quantumsim, branch: stable/v0
 import quantumsim.circuit as circuit
 import numpy as np
 import matplotlib.pyplot as plt
+from pymatching import Matching
+from scipy.linalg import toeplitz
+
 
 
 @dataclass
@@ -42,6 +45,10 @@ class RepCodeGenerator:
     @property
     def cbit_names(self):
         return [cb.name for cb in self.cbits]
+
+    @property
+    def matching_matrix(self):
+        return toeplitz([1] + [0] * (self.distance - 1), [1, 1] + [0] * (self.distance - 1))
 
     def generate_stabilizer_round(self, injected_errors=None, plot=False) -> circuit.Circuit:
         if injected_errors is not None:
@@ -151,61 +158,125 @@ class RepCodeGenerator:
             plt.show()
         return c
 
+    def generate_measure_data(self, plot=False):
+        c = self.init_circuit()
+        t_start = 0
+        t_moment = self.params.meas_duration
+
+        for q, cb in zip(self.qubit_names[::2], self.cbit_names[::2]):
+            c.add_gate(circuit.Measurement(q,
+                                           time=t_start + t_moment / 2,
+                                           sampler=self.sampler,
+                                           output_bit=cb
+                                           ))
+        t_start += t_moment
+        c.add_waiting_gates(0, t_start)
+        c.order()
+
+        if plot:
+            fig, ax = c.plot()
+            fig.set_figwidth(10)
+            plt.show()
+        return c
+
+    def generate_logical_X(self, plot=False):
+        c = self.init_circuit()
+        t_start = 0
+        t_moment = self.params.single_qubit_gate_duration
+
+        for q, cb in zip(self.qubit_names[::2], self.cbit_names[::2]):
+            c.add_gate(circuit.RotateX(q,
+                                       time=t_start + t_moment / 2,
+                                       angle=np.pi
+                                       ))
+        t_start += t_moment
+        c.add_waiting_gates(0, t_start)
+        c.order()
+
+        if plot:
+            fig, ax = c.plot()
+            fig.set_figwidth(10)
+            plt.show()
+        return c
+
 
 if __name__ == "__main__":
     # run simulation
     ########
-
-    cparams = CircuitParams(t1=10e5,
-                            t2=10e5,
+    plot = False
+    distance = 2
+    encoded_data = True
+    cparams = CircuitParams(t1=10e3,
+                            t2=10e3,
                             single_qubit_gate_duration=20,
                             two_qubit_gate_duration=40,
                             meas_duration=400,
                             reset_duration=250,
                             reset_latency=200)
 
-    repc = RepCodeGenerator(distance=2,
+    repc = RepCodeGenerator(distance=distance,
                             circuit_params=cparams
                             )
 
     state = quantumsim.sparsedm.SparseDM(repc.register_names)
     results = []
 
-    circ_no_err = repc.generate_stabilizer_round(plot=True)
+    stabilizer = repc.generate_stabilizer_round(plot=plot)
 
+    # start cycle
+    if encoded_data == True:
+        repc.generate_logical_X(plot=plot).apply_to(state)
     for i in range(3):
-        circ_no_err.apply_to(state)
-        results.append([state.classical[cb] for cb in repc.cbit_names])
+        stabilizer.apply_to(state)
+        results.append([state.classical[cb] for cb in repc.cbit_names[1::2]])
         # apply active reset
         to_reset = []
-        for q, cb in zip(repc.qubit_names, repc.cbit_names):
+        for q, cb in zip(repc.qubit_names[1::2], repc.cbit_names[1::2]):
             if state.classical[cb] == 1:
                 to_reset.append(q)
         repc.generate_active_reset(to_reset).apply_to(state)
 
-    repc.generate_bitflip_error(["0"], plot=True).apply_to(state)
+    repc.generate_bitflip_error(["0"], plot=plot).apply_to(state)
+
+    for i in range(1):
+        stabilizer.apply_to(state)
+        results.append([state.classical[cb] for cb in repc.cbit_names[1::2]])
+        # apply active reset
+        to_reset = []
+        for q, cb in zip(repc.qubit_names[1::2], repc.cbit_names[1::2]):
+            if state.classical[cb] == 1:
+                to_reset.append(q)
+        repc.generate_active_reset(to_reset).apply_to(state)
+
+    repc.generate_bitflip_error(["4"]).apply_to(state)
 
     for i in range(6):
-        circ_no_err.apply_to(state)
-        results.append([state.classical[cb] for cb in repc.cbit_names])
+        stabilizer.apply_to(state)
+        results.append([state.classical[cb] for cb in repc.cbit_names[1::2]])
         # apply active reset
         to_reset = []
-        for q, cb in zip(repc.qubit_names, repc.cbit_names):
+        for q, cb in zip(repc.qubit_names[1::2], repc.cbit_names[1::2]):
             if state.classical[cb] == 1:
                 to_reset.append(q)
         repc.generate_active_reset(to_reset).apply_to(state)
 
-    repc.generate_bitflip_error(["0"]).apply_to(state)
+    repc.generate_measure_data(plot=plot).apply_to(state)
+    data_meas = np.array([state.classical[cb] for cb in repc.cbit_names[::2]])
 
-    for i in range(6):
-        circ_no_err.apply_to(state)
-        results.append([state.classical[cb] for cb in repc.cbit_names])
-        # apply active reset
-        to_reset = []
-        for q, cb in zip(repc.qubit_names, repc.cbit_names):
-            if state.classical[cb] == 1:
-                to_reset.append(q)
-        repc.generate_active_reset(to_reset).apply_to(state)
-
+    # postprocessing
     results = np.array(results)
-    print(results.T)
+    detection_events = np.vstack([[0] * distance, np.logical_xor(results[1:], results[:-1])])
+    print("detection events")
+    print(detection_events.astype(int).T)
+    pauli_frame = Matching(repc.matching_matrix, repetitions=detection_events.shape[0]).decode(detection_events.T)
+    print("Pauli frame")
+    print(pauli_frame)
+    print("data qubits meas result")
+    print(data_meas)
+    recovered = np.logical_xor(data_meas, pauli_frame)
+    print("recovered state")
+    print(recovered.astype(int))
+    recovered_data = np.ceil(np.count_nonzero(recovered)/len(recovered)).astype(bool)
+    success = not recovered_data ^ encoded_data
+    print("success = ", success)
+
