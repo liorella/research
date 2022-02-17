3dcdb947d305f45d3674eef12d8c24f52f8a2267
Satya Bade <satya@quantum-machines.co>
2022-02-17 16:44:46 +0100

    add monte carlo experiment run

--- phys/qecsim/simulate_qec_rounds_stim.py
+++ phys/qecsim/simulate_qec_rounds_stim.py
@@ -109,6 +109,82 @@ def experiment_run(circuit: stim.Circuit,
     return success / shots
 
 
+def monte_carlo_experiment_run(code_task: str,
+                               distance: int,
+                               num_rounds: int,
+                               params: CircuitParams,
+                               shots: int,
+                               reset_strategy: str) -> float:
+
+    success = 0
+    for _ in range(shots):
+        circuit, context, weight = generate_scheduled(
+                code_task=code_task,
+                distance=distance,
+                rounds=num_rounds,
+                params=params
+            )
+
+        surf_circ_iter = to_measure_segments(circuit)
+
+        f_vec = np.zeros(len(context.active_ancillas), dtype=np.uint8)
+        results_prev = np.zeros_like(f_vec)
+        reset_indicator = np.zeros_like(f_vec)
+        results_record = np.zeros((num_rounds + 1, len(context.active_ancillas)), dtype=np.uint8)
+        sim = stim.TableauSimulator()
+
+        for i, segment in enumerate(surf_circ_iter):
+            if i < num_rounds:
+
+                # inject error
+                # if i == 1:
+                #     ec = stim.Circuit()
+                #     ec.append_operation('X', [10])
+                #     sim.do(ec)
+                #
+                # if i == 2:
+                #     ec = stim.Circuit()
+                #     ec.append_operation('X', [14])
+                #     sim.do(ec)
+
+                sim.do(gen_feedback_circuit(reset_indicator, context.active_ancillas, context))
+                results = do_and_get_measure_results(sim, segment, context.active_ancillas)
+                results_record[i, :] = results
+                if reset_strategy == 'APT':
+                    f_vec = (f_vec + results_prev) % 2
+                    results_prev = results
+                    reset_indicator = f_vec
+                elif reset_strategy == 'AR':
+                    reset_indicator = results
+                else:
+                    raise ValueError(f'unknown reset strategy {reset_strategy}')
+            elif i == num_rounds:
+                results_data = do_and_get_measure_results(sim, segment, context.data_qubits)
+                data_parity = context.matching_matrix @ results_data % 2
+                if reset_strategy == 'APT':
+                    # the following is because D_n = p_n + p_{n-1} and p_{n-1} = m_{n-1} + f_n
+                    results = (f_vec + data_parity + results_prev) % 2
+                    results_record[i, :] = results
+                    to_decode = results_record.T
+                elif reset_strategy == 'AR':
+                    results_record[i, :] = data_parity
+                    to_decode = np.diff(np.hstack((np.zeros_like(f_vec)[:, np.newaxis], results_record.T))) % 2
+                else:
+                    raise ValueError(f'unknown reset strategy {reset_strategy}')
+                frame = context.decode(to_decode)
+                # print('frame = ', frame)
+                # print('flipped qubits = ', np.array(context.data_qubits)[np.flatnonzero(frame)])
+                corrected_state = (results_data + frame) % 2
+                logical_state = context.logical_vecs @ corrected_state.T % 2
+                naive_logical = context.logical_vecs @ results_data % 2
+                # print(f'logical state = {logical_state} naive logical = {naive_logical}')
+                # print(to_decode)
+                if logical_state == 0:
+                    success += weight
+
+    return success / shots
+
+
 if __name__ == '__main__':
     cparams = CircuitParams(t1=15e3,
                             t2=10e3,
