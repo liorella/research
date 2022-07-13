16a584c0f55656bdc7456d5320aafe72dbf012fd
Lior Ella <lior@quantum-machines.co>
2022-07-13 21:56:50 +0300

    added qua example and modified quskit example

--- phys/tomo_qua.py
+++ phys/tomo_qua.py
@@ -15,7 +15,7 @@ backend = AerSimulator.from_backend(FakeParis())
 # Run experiments
 
 # GHZ State preparation circuit
-nq = 1
+nq = 2
 qc_ghz = qiskit.QuantumCircuit(nq)
 qc_ghz.h(0)
 qc_ghz.s(0)
@@ -23,18 +23,20 @@ qc_ghz.s(0)
 #     qc_ghz.cx(0, i)
 
 # QST Experiment
-qstexp1 = StateTomography(qc_ghz)
+qstexp1 = StateTomography(qc_ghz, measurement_qubits=[0, 1])
 qstdata1 = qstexp1.run(backend, seed_simulation=100).block_for_results()
 
-from qiskit_experiments.library.tomography.qst_analysis import StateTomographyAnalysis
-
-sta = StateTomographyAnalysis()
-
-from qiskit_experiments.library.tomography.fitters import linear_inversion
-
-linv = linear_inversion(np.array([[100, 200], [50, 250], [30, 270]]),
-                        np.array([300, 300, 300]),
-                        np.array([[0], [1], [2]]),
-                        np.array([]),
-                        measurement_basis=PauliMeasurementBasis(),
-                        measurement_qubits=(0, ))
+print(qstdata1.analysis_results())
+print(qstdata1.metadata)
+# from qiskit_experiments.library.tomography.qst_analysis import StateTomographyAnalysis
+#
+# sta = StateTomographyAnalysis()
+#
+# from qiskit_experiments.library.tomography.fitters import linear_inversion
+#
+# linv = linear_inversion(np.array([[100, 200], [50, 250], [30, 270]]),
+#                         np.array([300, 300, 300]),
+#                         np.array([[0], [1], [2]]),
+#                         np.array([]),
+#                         measurement_basis=PauliMeasurementBasis(),
+#                         measurement_qubits=(0, ))
