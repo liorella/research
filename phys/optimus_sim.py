a4d7dfd6045440e653f25157796385b0d088cbba
Lior Ella <lior@quantum-machines.co>
2022-09-19 16:28:55 +0300

    WIP on optimus simulation

--- phys/optimus_sim.py
+++ phys/optimus_sim.py
@@ -1,27 +1,32 @@
 # simulate optimus traversal, based on https://arxiv.org/pdf/1803.03226.pdf
 
 # graph
+import logging
 from enum import Enum
-from logging import log
+
+log = logging.getLogger("optimus")
+log.addHandler(logging.StreamHandler())
 
 
-class State(Enum):
-    out_of_spec = 0
-    in_spec = 1
-    bad_data = 2
+class HiddenState(Enum):
+    uncalibrated = -1
+    ok = 0
+    drifted = 1  # calibration needed on this node only
+    corrupted = 2  # bad data due to some downstream node being drifted
+    failure = 3  # bad data, uncorrectable
 
 
 class Cal:
     def __init__(self, name, dependencies):
         self.name = name
-        self.state = State.out_of_spec
-        self.last_checked_ago = None
+        self.last_measured = None
         self.staleness = None
         self.dependencies = dependencies
-        self.cal_fails = None
+        self.hidden_state = HiddenState.uncalibrated
 
     def __repr__(self):
-        return f"<name: {self.name} | last checked: {self.last_checked_ago} | staleness: {self.staleness}>"
+        return f"<name: {self.name} | state: {self.hidden_state} | " \
+               f"last measured: {self.last_measured} | staleness: {self.staleness}>"
 
 
 class Result:
@@ -31,38 +36,51 @@ class Result:
         self.bad_data = None
 
 
-def check_state(node: Cal) -> Result:
+def check_state(node: Cal, mode='soft') -> Result:
+    log.info(f"checking state for {node}")
     res = Result()
-    stale = node.last_checked_ago > node.staleness
-    recalibrated_dependencies = any(d.last_checked_ago < node.last_checked_ago
+    stale = node.last_measured > node.staleness
+    recalibrated_dependencies = any(d.last_measured < node.last_measured
                                     for d in node.dependencies)
-    cal_failures = node.cal_fails
-    failing_dependencies = any(d.state == State.out_of_spec for d in node.dependencies)
-    res.success = not (stale or recalibrated_dependencies or cal_failures or failing_dependencies)
+    if mode == 'soft':
+        failing_dependencies = False
+    elif mode == 'strict':
+        failing_dependencies = any(not check_state(d).success for d in node.dependencies)  # recursive
+    else:
+        raise ValueError(f"unknown mode {mode}")
+    res.success = not (stale or recalibrated_dependencies or failing_dependencies)
     return res
 
 
 def check_data(node: Cal) -> Result:
+    log.info(f"checking data for {node}")
     res = Result()
-    if node.state == State.in_spec:
+    if node.hidden_state == HiddenState.ok:
         res.in_spec = True
         res.bad_data = False
-    elif node.state == State.out_of_spec:
+    elif node.hidden_state == HiddenState.drifted or node.hidden_state == HiddenState.uncalibrated:
         res.in_spec = False
         res.bad_data = False
-    elif node.state == State.bad_data:
+    elif node.hidden_state == HiddenState.corrupted or node.hidden_state == HiddenState.failure:
         res.in_spec = False
         res.bad_data = True
+    node.last_measured = 0
     return res
 
 
+class CalibrationError(Exception):
+    pass
+
+
 def calibrate(node: Cal) -> Result:
+    log.info(f"calibrating {node}")
     res = Result()
-    if node.cal_fails:
-        res.in_spec = State.bad_data  # NOT SURE
+    if node.hidden_state.value < 3:  # succeed to calibrate in all cases except failure
+        node.hidden_state = HiddenState.ok
     else:
-        res.in_spec = State.in_spec
-    return Result()
+        raise CalibrationError(f"calibration for node {node} failed")
+    node.last_measured = 0
+    return res
 
 
 def update_parameters(result: Result) -> None:
--- phys/test_optimus_sim.py
+++ phys/test_optimus_sim.py
@@ -1,7 +1,12 @@
 import math
 
-from optimus_sim import Cal, State, maintain
+from optimus_sim import Cal, maintain, HiddenState
+import logging
 
+log = logging.getLogger("optimus")
+log.setLevel(logging.INFO)
+
+# build graph
 rr_spec = Cal("rr spec", set())
 qubit_spec = Cal("q spec", {rr_spec})
 t1 = Cal("t1", {qubit_spec})
@@ -11,13 +16,56 @@ rb = Cal("rb", {t1, t2})
 nodes = {rr_spec, qubit_spec, t1, t2, rb}
 
 
-def test_uncalibrated_scenario():
+def test_ok_scenario():
+    # TODO: finish this
     for n in nodes:
-        n.state = State.out_of_spec
-        n.last_checked_ago = math.inf
+        n.last_measured = 2
         n.staleness = 3
+        n.hidden_state = HiddenState.ok
 
     maintain(rb)
+
+
+def test_uncalibrated_scenario():
+    for n in nodes:
+        n.last_measured = math.inf
+        n.staleness = 3
+        n.hidden_state = HiddenState.uncalibrated
+
+    maintain(rb)
+    print("")
     for n in nodes:
         print(n)
+    for n in nodes:
+        assert n.hidden_state == HiddenState.ok
+        assert n.last_measured == 0
 
+
+def test_q_spec_is_stale_no_drift():
+    """
+    only q spec is stale, refresh it in a scenario where all check_data passes
+    (nothing drifted)
+    """
+    for n in nodes:
+        n.last_measured = 2
+        n.staleness = 3
+        n.hidden_state = HiddenState.ok
+
+    qubit_spec.staleness = 1
+
+    maintain(rb)
+    print("")
+    for n in nodes:
+        print(n)
+    for n in nodes:
+        assert n.hidden_state == HiddenState.ok
+    for n in nodes.difference({rr_spec}):
+        assert n.last_measured == 0
+    assert rr_spec.last_measured == 2
+
+
+def test_q_spec_is_stale_with_drift():
+    raise NotImplementedError()
+
+
+# TODO: add more tests
