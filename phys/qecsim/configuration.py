3d53abb397397858cb9cfc0ea67d7163b40361fb
Yaniv Kurman <yanivk@quantum-machines.co>
2023-08-02 08:14:32 +0300

    Bar Illan tests

--- phys/qecsim/configuration.py
+++ phys/qecsim/configuration.py
@@ -10,17 +10,23 @@ config = {
         "con1": {
             "analog_outputs": {
                 1: {"offset": +0.0},
+                2: {"offset": 0.0}
             },
-            "digital_outputs": {1: {'inverted': True},
-                                2: {},
-                                3: {}},
+            "analog_inputs": {
+                1: {"offset": 0.0}
+            },
+            "digital_outputs": {3: {'inverted': True},
+                                4: {},
+                                5: {},
+                                9: {},
+                                10: {}},
         }
     },
     "elements": {
         "CS_n": {
             "digitalInputs": {
                 "digital_input1": {
-                    "port": ("con1", 1),
+                    "port": ("con1", 3),
                     "delay": 0,
                     "buffer": 0,
                 },
@@ -32,7 +38,7 @@ config = {
         "SCLK": {
             "digitalInputs": {
                 "digital_input1": {
-                    "port": ("con1", 2),
+                    "port": ("con1", 4),
                     "delay": 0,
                     "buffer": 0,
                 },
@@ -44,7 +50,45 @@ config = {
         "MOSI": {
             "digitalInputs": {
                 "digital_input1": {
-                    "port": ("con1", 3),
+                    "port": ("con1", 5),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            "operations": {
+                "send_bit": "start_data_transfer",
+            },
+        },
+        "MEAS_IN": {
+            "singleInput": {
+                'port': ('con1', 2),
+            },
+            'intermediate_frequency': 0,
+            'operations': {
+                'readout': 'readout_pulse'
+            },
+            "outputs": {
+                "out1": ("con1", 1)
+            },
+            "time_of_flight": 24,
+            "smearing": 0
+        },
+        "TOTRIG": {
+            "digitalInputs": {
+                "digital_input1": {
+                    "port": ("con1", 9),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            "operations": {
+                "to_trig": "clear_cs",
+            },
+        },
+        "TOMESURE": {
+            "digitalInputs": {
+                "digital_input1": {
+                    "port": ("con1", 10),
                     "delay": 0,
                     "buffer": 0,
                 },
@@ -70,10 +114,36 @@ config = {
             "length": cs_period,
             "digital_marker": "cs_n",
         },
+
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": 200,
+            "waveforms": {
+                "single": "zero_wf"
+            },
+            "integration_weights": {
+                "const": "const"
+            },
+            "digital_marker": "ON"
+        },
     },
     "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]},
         "set_high": {"samples": [(1, spi_clk_period)]},
         "clk": {"samples": clk_cycle*transaction_size_bits},
-        "cs_n": {"samples": [(1, cs_period)]}  # note that this is inverted
+        "to_trig": {"samples": [(1,0)]},
+        "cs_n": {"samples": [(1, cs_period)],}  # note that this is inverted
+    },
+    "waveforms": {
+        "zero_wf": {
+            "type": "constant",
+            "sample": 0.0,
+        }
+    },
+    "integration_weights": {
+        "const": {
+            "cosine": [(1.0, 200)],
+            "sine": [(0.0, 200)],
+        }
     },
 }
--- phys/qecsim/magneton.py
+++ phys/qecsim/magneton.py
@@ -7,12 +7,10 @@ from configuration import *
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 import stim
 import numpy as np
-use_simulator = True
-
+use_simulator = False
 # qmm = QuantumMachinesManager("tyler-263ed49e.dev.quantum-machines.co", port=443, credentials=create_credentials())
-qmm = QuantumMachinesManager(host='product-52ecaa43.dev.quantum-machines.co', port=443,
-                              credentials=create_credentials()) #cluser
-# qmm = QuantumMachinesManager(host="192.168.116.150", cluster_name='my_cluster_1') # OPX in research team room
+#qmm = QuantumMachinesManager(host='product-52ecaa43.dev.quantum-machines.co', port=443,credentials=create_credentials()) #cluser
+qmm = QuantumMachinesManager(host="192.168.116.123", cluster_name='my_cluster_2') # OPX in research team room
 ## calculate simple error probabilities
 relevant_detectors= np.array([0,1,2,3,5,7,8,10,12,13,14,15])
 
@@ -40,6 +38,7 @@ for i,ind in enumerate(relevant_detectors):
 print(rel_probs)
 
 
+th = -0.0005
 ##
 from qm.qua import Random
 num_rounds=4
@@ -49,49 +48,285 @@ with program() as prog:
     probs_init = declare(fixed, value=rel_probs[0:4])
     probs_mid = declare(fixed, value=rel_probs[4:8])
     probs_fin = declare(fixed, value=rel_probs[8:])
-    boo_vec = declare(bool, size=4)
-    boo_vec_final = declare(bool, size=4)
+    boo_vec = declare(int, size=4)
+    boo_vec_final = declare(int, size=4)
+    cond=declare(bool)
+    boo = declare(int)
+    boo_final = declare(int)
     i = declare(int)
+    out = declare(fixed)
+    assign(boo_final, 0)
+    assign(boo, 0)
     with for_(i, 0, i < num_rounds, i+1):
         with if_(i == 0):
             for j in range(4):
                 a = generator.rand_fixed()
-                assign(boo_vec[j], a<probs_init[j])
+                assign(cond, a<probs_init[j])
+                assign(boo, boo + (Cast.to_int(cond) << j))
                 b = generator.rand_fixed()
-                assign(boo_vec_final[j], b<probs_fin[j])
+                assign(cond, b<probs_fin[j])
+                assign(boo_final, boo_final + (Cast.to_int(cond) << j))
         with else_():
             for j in range(4):
                 a = generator.rand_fixed()
-                assign(boo_vec[j], a<probs_mid[j])
+                assign(cond, a<probs_init[j])
+                assign(boo, boo + (Cast.to_int(cond) << j))
         align()
-        play("clear", "CS_n", timestamp_stream="clear_play")
-        play("tick", "SCLK")
-        for j in range(4):
-            play("send_bit", "MOSI", condition=boo_vec[j])
-
+        with switch_(boo):
+            with case_(0):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5, "MOSI") #5 clock cycles is 1 bit
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+            with case_(1):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+            with case_(2):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+            with case_(3):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+            with case_(4):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+            with case_(5):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                wait(5,"MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+            with case_(6):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+            with case_(7):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+            with case_(8):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                wait(5, "MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+            with case_(9):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                wait(5,"MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+            with case_(10):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+            with case_(11):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+            with case_(12):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                wait(5, "MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+            with case_(13):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                wait(5,"MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+            with case_(14):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                wait(5,"MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+            with case_(15):
+                play("clear", "CS_n", timestamp_stream="clear_play")
+                play("tick", "SCLK")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+                play("send_bit", "MOSI")
+        assign(boo,0)
         with if_(i == (num_rounds-1)):
             align()
-            play("clear", "CS_n", timestamp_stream="clear_play")
-            play("tick", "SCLK")
-            for j in range(4):
-                play("send_bit", "MOSI", condition=boo_vec_final[j])
+            with switch_(boo_final):
+                with case_(0):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")  # 5 clock cycles is 1 bit
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                with case_(1):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                with case_(2):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                with case_(3):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                with case_(4):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                with case_(5):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                with case_(6):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                with case_(7):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                with case_(8):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(9):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(10):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(11):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(12):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(13):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(14):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    wait(5, "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                with case_(15):
+                    play("clear", "CS_n", timestamp_stream="clear_play")
+                    play("tick", "SCLK")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+                    play("send_bit", "MOSI")
+        wait(150)
 
-        wait(173)
-    wait_for_trigger()
-    measure("",integration)
-        # wait(spi_clk_period, "MOSI")
+    align()
+    wait_for_trigger("TOMESURE")
+    play("to_trig", "TOTRIG")
+    align()
+    with if_(boo_final > 4):
+        play("send_bit","TOMESURE")
+    measure("readout", "MEAS_IN", "signal", integration.full("const", out))
+    save(out, "out")
+    with if_(out<th):
+        play("send_bit", "MOSI")
+
+
 
 if use_simulator:
-    job = qmm.simulate(config, prog, SimulationConfig(int(250*(num_rounds+1))))  # in clock cycles, 4 ns
+    job = qmm.simulate(config, prog, SimulationConfig(int(300*(num_rounds+1))))  # in clock cycles, 4 ns
     samples = job.get_simulated_samples()
     # results = job.result_handles.get('all_bits')
     # print(results.fetch_all())
     # plot it
     print(job.result_handles.clear_play.fetch_all())
-    cs_n = samples.con1.digital['1'] + 3
-    clk = samples.con1.digital['2'] + 1.5
-    mosi = samples.con1.digital['3']
-
+    cs_n = samples.con1.digital['3'] + 3
+    clk = samples.con1.digital['4'] + 1.5
+    mosi = samples.con1.digital['5']
+    # trig = samples.con1.digital['10'] + 4.5
 
     plt.style.use('ggplot')
     fig, ax = plt.subplots()
@@ -99,6 +334,7 @@ if use_simulator:
     ax.plot(cs_n[200:], label="CS_N")
     ax.plot(clk[200:], label="CLK")
     ax.plot(mosi[200:], label="MOSI")
+    ax.plot(trig[200:], label="FROMTRIG")
     ax.legend()
     ax.set_yticks([])
     ax.yaxis.set_tick_params(labelleft=False)
@@ -112,7 +348,7 @@ if use_simulator:
 
 
 else:
-    qmm = QuantumMachinesManager(host="172.16.33.100", cluster_name="Cluster_83")
+    # qmm = QuantumMachinesManager(host="192.168.116.123", cluster_name='my_cluster_2')  # OPX in research team room
     qm = qmm.open_qm(config)
     job = qm.execute(prog)
 
